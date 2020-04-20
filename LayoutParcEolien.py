d2877eb1bbad76d0631dc95bcf33ceb91e4606c6
aubin-tchoi <60398825+aubin-tchoi@users.noreply.github.com>
2020-04-20 20:38:17 +0200

    Add files via upload

---
+++ LayoutParcEolien.py
@@ -0,0 +1,66 @@
+## Représentation de la disposition du parc éolien
+
+import os
+import re
+import matplotlib.pyplot as plt
+import docx2txt
+
+# Conversion du .docx en .txt
+
+MY_TEXT = docx2txt.process("/Users/aubin/Documents/1A/Lidar/Work/coordx_y_mat_eoliennes_ls.docx")
+
+with open("/Users/aubin/Documents/1A/Lidar/Work/coordx_y_mat_eoliennes_ls.txt", "w") as text_file:
+    print(MY_TEXT, file=text_file)
+
+# Fonctions utiles
+
+def skipline(n):
+    for i in range(n):
+        line = file.readline()
+
+def placepoint(color): # Suppose que les deux prochaines lignes contiennent les coordonnées du point à placer
+    line = file.readline()
+    x = float(line)
+    line = file.readline()
+    y = float(line)
+    plt.scatter(x,y,c=color)
+
+# Lecture du fichier
+
+path = "/Users/aubin/Documents/1A/Lidar/Work/coordx_y_mat_eoliennes_ls.txt"
+file = open(path)
+
+with open(path, "r+") as f:
+    d = f.readlines()
+    f.seek(0)
+    for i in d:
+        if i != '\n':
+            f.write(i)
+    f.truncate()
+
+skipline(2)
+placepoint('g')        # Mât en vert
+
+skipline(1)
+placepoint('r')        # Lidar en rouge
+
+line = file.readline()
+n = 8                  # Nombre d'éoliennes
+
+for i in range(n):     # Présenté sous le format : n°,x,y,z,45 donc on skip 1 ligne avant de placer le point et 2 après
+    skipline(1)
+    placepoint('b')    # Eoliennes en bleu
+    skipline(2)
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Layout du parc éolien (Mât en vert, lidar en rouge et éoliennes en bleu)')
+plt.grid()
+plt.axis('equal')
+plt.show()
+
+file.close()
+
+# Suppression du fichier .txt créé
+
+os.remove("/Users/aubin/Documents/1A/Lidar/Work/coordx_y_mat_eoliennes_ls.txt")
