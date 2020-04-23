777b5cd7e822d671587906d6edd6cfae237196db
aubin-tchoi <60398825+aubin-tchoi@users.noreply.github.com>
2020-04-23 22:33:17 +0200

    Update Layout.py

--- Layout.py
+++ Layout.py
@@ -2,13 +2,18 @@
 
 import os
 import re
-import matplotlib.pyplot as plt
 import docx2txt
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+
+sns.set(color_codes=True)
 
 # path = "/Users/aubin/OneDrive/1A/Lidar/Work/"
 
-def Layout(path,b):
-
+def Layout(path,show):
+    _, ax = plt.subplots()
     # Conversion du .docx en .txt
 
     MY_TEXT = docx2txt.process(path + "coordx_y_mat_eoliennes_ls.docx")
@@ -22,19 +27,19 @@ def Layout(path,b):
         for i in range(n):
             file.readline()
 
-    def placepoint(color): # Suppose que les deux prochaines lignes contiennent les coordonnées du point à placer
+    def placepoint(s,color):    # Suppose que les deux prochaines lignes contiennent les coordonnées du point à placer
         line = file.readline()
         x = float(line)
         line = file.readline()
         y = float(line)
-        plt.scatter(x,y,c=color)
+        ax.scatter(x, y, s = s, color = color, alpha = 0.75, linewidths = 0.5, edgecolors = 'k')
         return x,y
 
     # Lecture du fichier
 
     with open(path + "coordx_y_mat_eoliennes_ls.txt", "r+") as f:
         d = f.readlines()
-        f.seek(0)
+        f.seek(0)               # Place le curseur en 0
         for i in d:
             if i != '\n':
                 f.write(i)
@@ -43,17 +48,17 @@ def Layout(path,b):
     file = open(path + "coordx_y_mat_eoliennes_ls.txt")
 
     skipline(2)
-    placepoint('g')        # Mât en vert
+    placepoint(80,'g')          # Mât en vert
 
     skipline(1)
-    x,y = placepoint('r')  # Lidar en rouge
+    x,y = placepoint(90,'r')    # Lidar en rouge
 
     line = file.readline()
-    n = 8                  # Nombre d'éoliennes
+    n = 8                       # Nombre d'éoliennes
 
-    for i in range(n):     # Présenté sous le format : n°,x,y,z,45 donc on skip 1 ligne avant de placer le point et 2 après
+    for i in range(n):          # Présenté sous le format : n°,x,y,z,45 donc on skip 1 ligne avant de placer le point et 2 après
         skipline(1)
-        placepoint('b')    # Eoliennes en bleu
+        placepoint(60,'b')      # Eoliennes en bleu
         skipline(2)
 
     # Affichage
@@ -63,7 +68,7 @@ def Layout(path,b):
     plt.title('Layout du parc éolien (Mât en vert, lidar en rouge et éoliennes en bleu)')
     plt.grid()
     plt.axis('equal')
-    if b:
+    if show:
         plt.show()
 
     file.close()
