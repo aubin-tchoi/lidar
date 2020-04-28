653c2ce4b73e8e82667a767382c993f560097f05
aubin-tchoi <60398825+aubin-tchoi@users.noreply.github.com>
2020-04-28 15:23:13 +0200

    Update Comparaison.py

--- Comparaison.py
+++ Comparaison.py
@@ -1,7 +1,6 @@
 # Comparaison des valeurs de vent obtenues
 
 import numpy as np
-import pandas as pd
 
 # Calcul des coordonnées sphériques d'un point désigné par ses coordonnées cartésiennes
 
@@ -11,6 +10,10 @@ def cart_to_pol(x,y,z,xL,yL,zL):
     phi = np.arcsin((z-zL)/rho)*180/np.pi
     return rho, theta-180, phi
 
+# Calcul de la distance euclidienne entre deux points
+
+def distance(x,y,z,rho,theta,phi):
+    return np.sqrt((rho*sin(theta)*cos(phi) - x)**2 + (rho*cos(theta)*cos(phi) - y)**2 + (rho*sin(theta) - z)**2)
 
 # Renvoit un vecteur contenant la composante radiale du vent mesuré par l'anémomètre
 
@@ -22,6 +25,21 @@ def Projection(U,V,W,x,y,z,xL,yL,zL):
         R.append(U[k]*np.sin(theta)*np.cos(phi) + V[k]*np.cos(theta)*np.cos(phi) + W[k]*np.sin(phi))
     return R
 
+# Supposons qu'on ait trouvé les 8 points les plus proches du mât parmi les points mesurés par le Lidar,
+# Il faut alors moyenner les valeurs de vitesses en chacun de ces points
+# Cette moyenne doit rendre compte de la position du mât dans le polygône courbé reliant ces points.
+
+def moyenne(L, C, x, y, z):    # C correspond ici à l'ensemble des points
+    d = [distance(x,y,z,L[5][k],L[3][k],L[4][k]) for k in C] # Distance euclidienne
+    dtot = sum(d)
+    V = 0
+    for k in C:
+        V += L[5][k]/len(C)    # Moyenne pondérée par d
+    print(d)
+    return V
+
+"""
+# 1ère approche : si le pas de discrétisation des mesures est régulier, il suffit de trouver le cube dans lequel se trouve le mât, ce qui est assez facile à la difficulté près que certaines mesures peuvent manquer
 
 # Les deux fonctions Pas renvoient un tuple correspondant aux intervalles entre chaque mesure de r, de theta ou de phi (min(a,b) pour a,b dans liste tels que a != b)
 
@@ -45,21 +63,20 @@ def Pas_quicksort(L):  # Cette version utilise un quicksort
 
     r0, theta0, phi0 = [], [], []   # Contiendront les différentes valeurs de r, theta et phi (sans doublon)
     for k in range(len(L[0])):
-        if L[1][k] != 238:
-            if L[5][k] not in r0:
-                r0.append(L[5][k])
-            if L[3][k] not in theta0:
-                theta0.append(L[3][k])
-            if L[4][k] not in phi0:
-                phi0.append(L[4][k])
+        if L[5][k] not in r0:
+            r0.append(L[5][k])
+        if L[3][k] not in theta0:
+            theta0.append(L[3][k])
+        if L[4][k] not in phi0:
+            phi0.append(L[4][k])
 
     r, theta, phi = quicksort(r0), quicksort(theta0), quicksort(phi0)
-    """
+
     # Le plus petit écart se trouvera nécessairement entre 2 valeurs consécutives après quicksort
-    dr     = min([r[k] - r[k-1] for k in range(2,len(r))])
-    dtheta = min([theta0[k] - theta0[k-1] for k in range(2,len(theta0))])
-    dphi   = min([phi[k] - phi[k-1] for k in range(2,len(phi))])
-    """
+    # dr     = min([r[k] - r[k-1] for k in range(2,len(r))])
+    # dtheta = min([theta0[k] - theta0[k-1] for k in range(2,len(theta0))])
+    # dphi   = min([phi[k] - phi[k-1] for k in range(2,len(phi))])
+
     dr, dtheta, dphi = r[1] - r[0], theta[1] - theta[0], phi[1] - phi[0]
     return [dr, dtheta, dphi]
 
@@ -67,13 +84,12 @@ def Pas(L): # Cette version n'utilise pas de quicksort
 
     r0, theta0, phi0 = [], [], []   # Contiendront les différentes valeurs de r, theta et phi (sans doublon)
     for k in range(len(L[0])):
-        if L[1][k] != 238:
-            if L[5][k] not in r0:
-                r0.append(L[5][k])
-            if L[3][k] not in theta0:
-                theta0.append(L[3][k])
-            if L[4][k] not in phi0:
-                phi0.append(L[4][k])
+        if L[5][k] not in r0:
+            r0.append(L[5][k])
+        if L[3][k] not in theta0:
+            theta0.append(L[3][k])
+        if L[4][k] not in phi0:
+            phi0.append(L[4][k])
 
     dr     = (max(r0) - min(r0))/(len(r0)-1)    # len(r0)-1 puisque l'on compte le nombre d'intervalles
     dtheta = (max(theta0) - min(theta0))/(len(theta0)-1)
@@ -87,56 +103,31 @@ def test_pas_regulier(L):
     dr, dtheta, dphi = Pas(L)
     bool = [True, True, True]
     for k in range(N):
-        if L[1][k] != 238:
-            if abs(L[5][k]/dr - int(L[5][k]/dr)) > 0.001:   # L[5][k]/dr est entier si sa partie décimale est égale à 0
-                print("r n'évolue pas par pas réguliers")
-                print("Pas : " + str(dr) + ", " + "r = " + str(L[5][k]))
-                bool[0] = False
-                break
+        if abs(L[5][k]/dr - int(L[5][k]/dr)) > 0.001:   # L[5][k]/dr est entier si sa partie décimale est égale à 0
+            print("r n'évolue pas par pas réguliers")
+            print("Pas : " + str(dr) + ", " + "r = " + str(L[5][k]))
+            bool[0] = False
+            break
     else:
         print("r évolue par pas réguliers")
     for k in range(N):
-        if L[1][k] != 238:
-            if abs(L[3][k]/dtheta - int(L[3][k]/dtheta)) > 0.001:
-                print("theta n'evolue pas par pas réguliers")
-                print("Pas : " + str(dphi) + ", " + "theta = " + str(L[3][k]))
-                bool[1] = False
-                break
+        if abs(L[3][k]/dtheta - int(L[3][k]/dtheta)) > 0.001:
+            print("theta n'evolue pas par pas réguliers")
+            print("Pas : " + str(dphi) + ", " + "theta = " + str(L[3][k]))
+            bool[1] = False
+            break
     else:
         print("theta évolue par pas réguliers")
     for k in range(N):
-        if L[1][k] != 238:
-            if abs(L[4][k]/dphi - int(L[4][k]/dphi)) > 0.001:
-                print("phi n'evolue pas par pas réguliers")
-                print("Pas : " + str(dphi) + ", " + "phi = " + str(L[4][k]))
-                bool[2] = False
-                break
+        if abs(L[4][k]/dphi - int(L[4][k]/dphi)) > 0.001:
+            print("phi n'evolue pas par pas réguliers")
+            print("Pas : " + str(dphi) + ", " + "phi = " + str(L[4][k]))
+            bool[2] = False
+            break
     else:
         print("phi évolue par pas régulier")
     return bool
 
-def s(x):
-    if x == 0:
-        return 5
-    elif x == 1:
-        return 3
-    elif x == 2:
-        return 4
-
-# Supposons qu'on ait trouvé les 8 points les plus proches du mât parmi les points mesurés par le Lidar,
-# Il faut alors moyenner les valeurs de vitesses en chacun de ces points
-# Cette moyenne doit rendre compte de la position du mât dans le polygône courbé reliant ces points.
-
-def moyenne(L, C, x, y, z):    # C correspond ici à l'ensemble des points
-    d = [np.sqrt((L[5][k]*np.sin(L[3][k])*np.cos(L[4][k])-x)**2 + (L[5][k]*np.cos(L[3][k])*np.cos(L[4][k])-y)**2 + (L[5][k]*np.sin(L[4][k])-z)**2) for k in C] # Distance euclidienne
-    dtot = sum(d)
-    V = 0
-    for k in C:
-        V += L[5][k]/len(C)    # Moyenne pondérée par d
-    print(d)
-    return V
-
-"""
 # Renvoit la valeur de vitesse Lidar au niveau du mât
 
 def Interpolation_pas_regulier(L,x,y,z,xL,yL,zL):
@@ -144,22 +135,20 @@ def Interpolation_pas_regulier(L,x,y,z,xL,yL,zL):
     N = len(L[0])
     C = []  # Liste des points proches du mât
     for k in range(N):
-        if L[1][k] != 238:
-            if abs(L[5][k] - rho) <= dr/2 and abs(L[3][k] - theta) <= dtheta/2 and abs(L[4][k] - phi) <= dphi/2:
-                C.append(k)
+        if abs(L[5][k] - rho) <= dr/2 and abs(L[3][k] - theta) <= dtheta/2 and abs(L[4][k] - phi) <= dphi/2:
+            C.append(k)
     return moyenne(L, C, x, y, z)
+"""
 
-
+# 2ème approche : on détermine directement les 8 points les plus proches du mât
 
 def Interpolation8(L,x,y,z,xL,yL,zL):
     rho, theta, phi = cart_to_pol(x,y,z,xL,yL,zL)
     C = [k for k in range(8)]   # Contient les indices des 8 points plus proches du mât
     V = 0
     for k in range(8,len(L[0])):    # Complexité en O(N) (On peut optimiser mais on ne passera pas en dessous de N)
-        if L[1][k] != 238:
-            for i in C:
-                if abs(L[5][k] - rho) < abs(L[5][i] - rho) and abs(L[3][k] - theta) < abs(L[3][i] - theta) and abs(L[4][k] - phi) < abs(L[4][i] - phi):
-                    i = k
-                    break   # On veut que C contienne des indices tous différents
+        for i in C:
+            if abs(L[5][k] - rho) < abs(L[5][i] - rho) and abs(L[3][k] - theta) < abs(L[3][i] - theta) and abs(L[4][k] - phi) < abs(L[4][i] - phi):
+                i = k
+                break   # On veut que C contienne des indices tous différents
     return moyenne(L, C, x, y, z)
-"""
