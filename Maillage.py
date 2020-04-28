daab01db97259e8641fc74beb0c2f984c2a0234e
aubin-tchoi <60398825+aubin-tchoi@users.noreply.github.com>
2020-04-28 21:30:30 +0200

    Update Maillage.py

--- Maillage.py
+++ Maillage.py
@@ -4,13 +4,20 @@ from numpy import cos, sin, pi
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# n : Nombre de points tracés
+# s : rayon des points
+# pause : durée de la pause entre chaque tracé de point
+# xL, yL, zL : coordonnées du Lidar
+# xM, yM, zM : coordonnées du mât
+
 def Maillage(L,n,s,pause,xL,yL,zL,xM,yM,zM):
     _, (ax1, ax2) = plt.subplots(1, 2, sharex = True)
-    ax1.set_title("y,x")
-    ax2.set_title("z,x")
+    ax1.set_title("y, x")
+    ax2.set_title("z, x")
     ax1.scatter(xM, yM, s = 2*s, color = 'g', alpha = 0.75)
     ax2.scatter(xM, zM, s = 2*s, color = 'g', alpha = 0.75)
     for i in range(n):
+        i = 17*i
         rho = L[5][i]
         theta = pi*(L[3][i]+180)/180
         phi = pi*L[4][i]/180
