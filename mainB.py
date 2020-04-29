c4a90d909fc69e87aba4c151de755930ae6d2cd6
aubin-tchoi <60398825+aubin-tchoi@users.noreply.github.com>
2020-04-29 18:38:12 +0200

    Update mainB.py

--- mainB.py
+++ mainB.py
@@ -25,14 +25,17 @@ from Maillage import Maillage
 # from Comparaison import Regular_steps, Interpolation_regular_steps
 # from Windrose import *
 
-path0  = path  + "Lidar+Sonic/"
+path0 = path + "Lidar+Sonic/"
 
 S = []
 R = []
 R_avg = []
 R_sigma = []
 
-for indice in range(1,9):
+os.chdir(path0)
+n = len(os.listdir())//2 # On compte le nombre de fichiers (de couples de fichiers Lidar/Sonique)
+
+for indice in range(1, n+1): # On parcourt les différents fichiers
 
     path1  = path0 + "151030" + str(indice) + ".I55"
     path2  = path0 + "WLS200s-15_radial_wind_data_2015-04-13_0" + str(indice) + "-00-00.csv"
@@ -83,3 +86,21 @@ for indice in range(1,9):
         print(str(indice) + "er fichier traité !")
     else:
         print(str(indice) + "ème fichier traité !")
+
+file = open(path0 + "Results.txt","w+")
+file.write("Compilation of the data extracted from the files in this directory \r\n")
+file.write("Average values of the radial wind speed (RWS) measured by Sonic and standard deviation associated for each set of data (DRWS) (m/s) \n")
+for ks in range(n):
+    file.write(str(R_avg[ks]) + " " + str(R_sigma[ks]) + "\n")
+file.write("\n")
+file.write("Values of the wind speed measured by Lidar at the location of the Sonic measuring device for each set of data (m/s) \n")
+for kl in range(n):
+    file.write(str(S[kl]) + "\n")
+file.write("\n")
+file.write("Values of the radial wind speed measured by Sonic \r\n")
+for ks1 in range(len(R)):
+    file.write("Day" + " " + str(ks1 + 1) + "\n")
+    for ks2 in range(len(R[0])):
+        file.write(str(R[ks1][ks2]) + " ")
+    file.write("\r\n")
+file.close()
