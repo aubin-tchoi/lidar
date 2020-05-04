edebb72c2f831c715391c680d4382e261f2fd693
aubin-tchoi <60398825+aubin-tchoi@users.noreply.github.com>
2020-05-04 17:56:55 +0200

    Update Parseur.py

--- Parseur.py
+++ Parseur.py
@@ -4,8 +4,15 @@ import os
 import numpy as np
 from shutil import copyfile
 
-# path1 : adresse du fichier 1510301.I55.txt
-# path2 : adresse du fichier WLS200s-15_radial_wind_data_2015-04-13_01-00-00.csv
+
+# Conversion du timestamp en dixièmes de seconde écoulés depuis minuit (pour match les indices des données Sonic)
+
+def convertime(str):
+    HouMouS = str.replace(":", " ").split() # Hour, Min, Sec
+    return (float(HouMouS[0])*3600 + float(HouMouS[1])*60 + float(HouMouS[2]))*10
+
+
+# Prend en entrée un fichier contenant des données Sonic et renvoit les array U, V, W (coordonnées du vent)
 
 def ParseurSonique(path):
 
@@ -28,50 +35,33 @@ def ParseurSonique(path):
 
     file.close()
     os.remove(path + ".txt")
+
     return U, V, W
 
+
+# Prend en entrée un fichier contenant des données Lidar et renvoit un array [temps, rho, theta, phi, RWS, DRWS]
+# Le temps est exprimé en dixièmes de s, rho en m, les angles en ° et les vitesses en m/s.
 
 def ParseurLidar(path):
 
     file = open(path,'r')
-
     line = file.readline()                  # On passe la première ligne
-
     n = len(open(path).readlines()) - 1     # Nombre de lignes dans le .csv (moins la première ligne)
 
-    line  = file.readline()
-    polar = line.replace(';',' ').replace(',',' ').split()
-    k     = len(polar)                      # Nombre de valeurs à extraire de chaque ligne (détaillées dans la première ligne)
+    time0, rho0, theta0, phi0, rws0, drws0  = [], [], [], [], [], []
 
-    intorfloat = [2,3,4,13]                 # Certaines données sont entières (les IDs)
-
-    # L = [Configuration ID, Scan ID, LOS ID, Azimuth (en °), Elevation (en °), Range (en m), RWS (en m/s), DRWS (en m/s), CNR (en dB), Confidence Index (en %), Mean Error, Status]
-    # RWS  : Radial Wind Speed
-    # DRWS : Dispersion of Radial Wind Speed
-    # CNR  : Carrier to Noise Ratio
-
-    # Azimuth   : angle entre l'objet et une direction de référence (Nord) dans le plan horizontal (theta)
-    # Elévation : (phi)
-
-    L = []
-    for j in range(2,k):                    # On doit décaler de 2 puisque le timestamp comprend un espace et qu'il prend deux places dans le split
-                                            # Si besoin je peux ajouter une colonne pour le timestamp, mais il faudra me préciser le format
-        if j in intorfloat:
-            L.append([int(polar[j])])
-        elif j not in intorfloat:
-            L.append([float(polar[j])])
-
-    for i in range(n-1):
+    for i in range(n):
         line = file.readline()
         polar = line.replace(';',' ').replace(',',' ').split()
-        if int(polar[3]) != 238:            # Un Scan ID égal à 238 correspond à une donnée non valide
-            for j in range(2,k):
-                if j in intorfloat:
-                    L[j-2].append(int(polar[j]))
-                elif j not in intorfloat:
-                    L[j-2].append(float(polar[j]))
-            continue
+        if convertime(polar[1]) < 72000: # time0 correspondra à l'indice de la ligne correspondante dans les mesures Sonic (indice*10 = nbr de s écoulées depuis minuit)
+            time0.append(convertime(polar[1]))
+            rho0.append(float(polar[7]))
+            theta0.append(float(polar[5]) + 180.)
+            phi0.append(float(polar[6]))
+            rws0.append(float(polar[8]))
+            drws0.append(float(polar[9]))
 
+    L = [np.array(time0), np.array(rho0), np.array(theta0), np.array(phi0), np.array(rws0), np.array(drws0)]
     file.close()
 
     return np.array(L)
