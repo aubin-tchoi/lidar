9a9bcf725de980b406affda7443419465b5ce547
aubin-tchoi <60398825+aubin-tchoi@users.noreply.github.com>
2020-05-22 18:26:09 +0200

    mean and Lidar values are now optionnal in Histo function

--- Interpret.py
+++ Interpret.py
@@ -30,7 +30,7 @@ def MeasuringTime(L):
 
 # Visualisation d'un histogramme des valeurs de vitesses radiales mesurées par l'anémomètre
 
-def Histo(R, R_avg, VL, n): # n : nombre de barres
+def Histo(R, n, R_avg = False, VL = False): # n : nombre de barres
 
     plt.figure("Histogramme", figsize = (14,14))
     H = np.zeros(n)
@@ -51,19 +51,24 @@ def Histo(R, R_avg, VL, n): # n : nombre de barres
     plt.bar(np.linspace(rmin,rmax,n), H, width = 4/n)
 
     # Ajout d'une barre rouge pour la moyenne
-    M = np.zeros(2*n)
-    M[int(2*n*(R_avg - rmin)/(rmax - rmin))] = max(H)
-    plt.bar(np.linspace(rmin,rmax,2*n), M, color = 'r', width = 2/n)
+
+    if not isinstance(R_avg, bool):
+        M = np.zeros(2*n)
+        M[int(2*n*(R_avg - rmin)/(rmax - rmin))] = max(H)
+        plt.bar(np.linspace(rmin,rmax,2*n), M, color = 'r', width = 2/n)
 
     # Ajout de barres vertes pour le Lidar
-    try:
-        li = np.zeros(2*n)
-        for v in VL:
-            if v < rmax:
-                li[int(2*n*(v - rmin)/(rmax - rmin))] += 2*max(H)/len(VL)
-        plt.bar(np.linspace(rmin,rmax,2*n), li, color = 'g', width = 2/n)
-    except IndexError:
-        print("Some of the RWS values measured by the Lidar are out of the interval [min(R),max(R)]")
+
+    if not isinstance(VL, bool):
+        try:
+            li = np.zeros(2*n)
+            for v in VL:
+                if v < rmax:
+                    li[int(2*n*(v - rmin)/(rmax - rmin))] += 2*max(H)/len(VL)
+            plt.bar(np.linspace(rmin,rmax,2*n), li, color = 'g', width = 2/n)
+        except IndexError:
+            print("Some of the RWS values measured by the Lidar are out of the interval [min(R),max(R)]")
+
     plt.xlabel("Répartition des valeurs de vitesse radiale mesurées par l'anémomètre Sonic")
     k = 1 # Nombre de décimales affichées en abscisses
     plt.xticks(np.linspace(rmin,rmax,int(n/2)), [str(round(10**k*el)/10**k) for el in np.linspace(rmin,rmax,int(n/2))])
